d2d31ce177098d7adc5dcd22712063a1ade26d11
Julian Mazzitelli <mazzitell.julian@gmail.com>
2015-04-12 15:48:36 -0400

    defined Vertex struct

--- mesh.c
+++ mesh.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "mesh.h"
 
 void draw() {
   printf("drawing\n");
+
+  struct Vertex *v = malloc(sizeof(struct Vertex));
+
+  v->x = 5;
+  printf("%d\n", v->x);
 }
--- mesh.h
+++ mesh.h
@@ -1,6 +1,11 @@
 #ifndef mesh
 #define mesh
 
+typedef struct Vertex {
+  int x;
+  int y;
+} Vertex;
+
 int mesh_test();
 void draw();
 
